9b434593bf6c542a66b0e94e96fd12d77e8e9675
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-09-26 18:58:18 -0700

    Added a Set ACAO to res.json's

--- app.js
+++ app.js
@@ -13,19 +13,24 @@ app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.get("/Todo", async (rec, res) => {
   const todos = await Todo.find();
-  res.json({ todos: todos });
+  res.json({ todos: todos }).set("Access-Control-Allow-Origin", "*");
 });
 
 app.post("/add-todo", async (rec, res) => {
   await Todo.create(req.body);
-  res.json({ todo: todo });
+  res.json({ todo: todo }).set("Access-Control-Allow-Origin", "*";
 });
+
 app.post("/edit-item/:id", async (rec, res) => {
   await Todo.findOneAndUpdate({ id: req.params.id }, req.body);
+  res.json({ message: "Item updated successfully" }).set("Access-Control-Allow-Origin", "*");
 });
+
 app.delete("/DELETE-item/:id", async (rec, res) => {
   await Todo.deleteOne({ id: req.params.id });
+  res.json({ message: "Item deleted successfully" }).set("Access-Control-Allow-Origin", "*");
 });
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
