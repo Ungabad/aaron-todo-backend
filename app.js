5cc8327897ee864002740d6f6162012e4eabe110
Aaron Dalager <113053870+Ungabad@users.noreply.github.com>
2024-10-01 17:00:48 -0700

    CORS!!?!!

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ mongoose.connect(
   `mongodb+srv://${process.env.MONGODB_API_KEY}@cluster0.zt5pw.mongodb.net/`
 );
 
-app.use(cors({}));
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 app.get("/Todo", (req, res) => {
